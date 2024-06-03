c81ab4ae193ced5260605bcabc511c1f3c06ab3e
greatoldowl <79207366+greatoldowl@users.noreply.github.com>
2024-06-04 02:48:08 +0300

    SaveLoad base

---
+++ SMOOTA/Assets/Scripts/SaveLoad.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class PlayerData
+{
+    public int health;
+    public int souls;
+    public int solidsouls;
+    public Vector3 position;
+
+}
+
+public class SaveLoad : MonoBehaviour
+{
+    PlayerData playerData;
+    string saveFilePath;
+    void Start()
+    {
+    playerData = new PlayerData();
+    playerData.health = 100;
+    playerData.souls = 5;
+        playerData.solidsouls = 3;
+    playerData.position = Vector3.zero;
+    saveFilePath = Application.persistentDataPath + "/PlayerData.json";
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape)) { }
+    }
+    public void SaveGame()
+    {
+        string savePlayerData = JsonUtility.ToJson(playerData);
+        File.WriteAllText(saveFilePath, savePlayerData);
+        Debug.Log("YOU SAVED");
+    }
+    public void LoadGame()
+    {
+        if (File.Exists(saveFilePath))
+        {
+            string loadPlayerData = File.ReadAllText(saveFilePath);
+            playerData = JsonUtility.FromJson<PlayerData>(loadPlayerData);
+            Debug.Log("Load game complete! \nPlayer health:" + playerData.health + ", Player souls:" + playerData.souls + ", Player solidsouls:" + playerData.solidsouls + ", Player Position:" + playerData.position);
+
+        }
+        else
+        {
+            Debug.Log("You, stupid bollock, there is nothing to load!");
+        }
+    }
+    public void DeleteSaveFile()
+    {
+        if (!File.Exists(saveFilePath))
+        {
+            File.Delete(saveFilePath);
+            Debug.Log("YOU DELETED YOUR SAVE, NOW FACE YOUR DOOM");
+        }
+        else
+        {
+            Debug.Log("YOU CAN'T DELETE SOMETHING, WHICH NOT EXIST");
+        }
+    }
+    // Update is called once per frame
+  
+}
